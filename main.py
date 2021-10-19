989ed47105fae26340a404de77a697e784a6dba7
Longxiao Zhang <zhanglongx@gmail.com>
2021-10-19 15:09:18 +0800

    main.py: token in mara.yaml

--- main.py
+++ main.py
@@ -14,8 +14,6 @@ def main():
 
     opt.add_argument('-c', '--compare', action='store_true', default=False, 
                         help='to compare with industry average')
-    opt.add_argument('-k', '--token', type=str, required=True,
-                        help='tushare token')
     opt.add_argument('-t', '--template', type=str, default="mara.yaml",
                         help='template file in .yaml [mara.yaml]')
     opt.add_argument('symbols', type=str, nargs='+', help='symbols input')
@@ -25,13 +23,19 @@ def main():
     with open(arg.template, "r") as stream:
         try:
             template = yaml.safe_load(stream)
-            if not 'plots' in template:
-                fatal("template file doesn't contain plots")
+            if not 'plots' in template or not 'global' in template:
+                fatal("template file doesn't contain plots/global")
 
         except yaml.YAMLError:
             fatal('parse template file error')
 
-    data = Data(arg.symbols, arg.token, compare=arg.compare)
+    globalSettings = template.pop('global', None)
+    token = globalSettings.pop('token', None)
+    if token is None:
+        fatal('global: missing token')
+    
+    data = Data(arg.symbols, token, compare=arg.compare)
+
     p = Plot()
     for t in template['plots']:
         name = t.pop('name', None)
