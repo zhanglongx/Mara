563ecc8a133496390473cc1f943e96cf7277a08c
Longxiao Zhang <zhanglongx@gmail.com>
2022-04-16 11:43:59 +0800

    remove: basic.py, dump.py

--- modules/_generic.py
+++ modules/_generic.py
@@ -1,7 +1,4 @@
-import re
 import pandas as pd
-import matplotlib.pyplot as plt
-import warnings
 
 import mara
 
@@ -10,98 +7,13 @@ class GenericMod(mara.ModuleProtocol):
     generic module
     '''
 
-    def __init__(self, indicator, fields, **kwargs) -> None:
-        if indicator is None or not isinstance(indicator, str):
-            raise ValueError('indicator should be one of tushare api')
+    def __init__(self, **kwargs) -> None:
+        pass
 
-        self.indicator = indicator
-
-        if fields is None: 
-            raise ValueError('fields cannot be None')
-        elif isinstance(fields, str):
-            fields = [fields]
-        elif len(fields) == 0:
-            raise ValueError('fields cannot be empty')
-
-        self.fields = fields
-        self.plot_params = kwargs
-
-        # get will output here
-        self.df = None
-
-    def init(self, api, ts_code, start_date=None, end_date=None, **kwargs) -> None:
-        super().init(api, ts_code, start_date, end_date)
+    def init(self, ts, ts_code, start_date=None, end_date=None, **kwargs) -> None:
+        super().init(ts, ts_code, start_date, end_date)
         return
 
-    # TODO: ttm
-    def get(self, ttm=False) -> pd.DataFrame:
-        # FIXME:
-        if self.indicator == 'fina_indicator':
-            api = self.ts.fina_indicator
-        elif self.indicator == 'balancesheet':
-            api = self.ts.balancesheet
-        else:
-            raise NotImplementedError()
-
-        if isinstance(self.fields, dict):
-            fields = self.fields.keys()
-            # TODO: _formula(), note fields is list now!
-            raise NotImplementedError()
-        else:
-            fields = self.fields
-
-        result = list()
-        for s in self.ts_code:
-            df = apiWrapper(api, ts_code=s, index='end_date',
-                            start_date=self.start_date, end_date=self.end_date, 
-                            fields=fields)
-
-            result.append(df)
-
-        self.df = pd.concat(result, axis=1, keys=self.ts_code) 
-        return self.df
-    
-    def _formula(self, report, catalog=None):
-        """
-        Calculate a report Dataframe based on formulas.
-        Hacks: using exec to formula, *DON'T rename parameter 'report'*
-        """
-
-        if isinstance(catalog, str):
-            # nothing to calculate
-            return report[catalog]
-        elif isinstance(catalog, dict):
-            pass
-        else:
-            raise TypeError('formula type is not supported')
-
-        # Hacks: 
-        for k in catalog.keys():
-            left = k
-            right = re.sub(r'([^- %+*\/\(\)\d]+)',
-                           r'report["\1"]',
-                           catalog[k])
-
-            exec('report["%s"] = %s' % (left, right))
-
-        # FIXME:
-        return report[list(catalog.keys())[0]]
-
-    def plot(self) -> None:
-        if self.plot_params is None:
-            plot_params = self.plot_params
-        else:
-            plot_params = dict()
-
-        if self.df is None:
-            warnings.warn('no dataframe, automatilly get...' )
-            self.get()
-
-        df = self.df
-        for f in df.columns.levels[1]:
-            kind = plot_params[f] if f in plot_params else 'line'
-
-            select = df.xs(f, axis=1, level=1)
-            select.plot(title=f, kind=kind)
-
-        plt.show()
+    # FIXME: ttm
+    def get(self, ttm=False, **kwargs) -> pd.DataFrame:
+        pass
--- modules/basic.py
+++
@@ -1,4 +0,0 @@
-from modules._generic import GenericMod
-
-# TODO
-MODULE=GenericMod('fina_indicator', ['longdeb_to_debt', 'ca_to_assets'])
